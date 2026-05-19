6f0615915e8fa5a68bef5743a4817de3c3a9077b
Brian Middleton <brian@planetargon.com>
2026-05-18 22:02:02 -0700

    Initial Commit

---
+++ Squirrel Away/Squirrel_AwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squirrel_AwayApp.swift
+//  Squirrel Away
+//
+//  Created by Brian Middleton on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Squirrel_AwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
